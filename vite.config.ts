3dfb681421c5e1e69dbf5fa1ff2c4f1b33f84b39
DiverseRaul <goraulseixas@gmail.com>
2025-03-18 19:22:36 -0400

    v1.4

--- vite.config.ts
+++ vite.config.ts
@@ -10,36 +10,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/',
+  base: '/BimbaBolao/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    assetsDir: '',
     minify: 'terser',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/js/[name].[hash].js',
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/\.(png|jpe?g|gif|svg|webp|ico)$/.test(assetInfo.name)) {
-            return `assets/images/[name].[hash].[ext]`
-          }
-          if (/\.(css)$/.test(assetInfo.name)) {
-            return `assets/css/[name].[hash].[ext]`
-          }
-          if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.name)) {
-            return `assets/fonts/[name].[hash].[ext]`
-          }
-          return `assets/[name].[hash].[ext]`
-        }
-      }
-    }
-  },
-  server: {
-    fs: {
-      strict: false
-    }
+    sourcemap: false
   }
 })
