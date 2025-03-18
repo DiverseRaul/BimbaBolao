327fd36aa4f240dd5d256b7ebec9c0a5b3bae56e
DiverseRaul <goraulseixas@gmail.com>
2025-03-18 19:27:28 -0400

    VReset

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
-  base: '/BimbaBolao/',
-  build: {
-    outDir: 'dist',
-    assetsDir: '',
-    minify: 'terser',
-    sourcemap: false
+  server: {
+    port: 3000
   }
 })
